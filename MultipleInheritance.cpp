1b26349b028720e272680a4e085289bb0b54a5a3
Re-taro <aminokacho@gmail.com>
2021-10-05 18:42:12 +0900

    :seedling: レポートの1のコード
    一分余分な書き方されてる

--- MultipleInheritance.cpp
+++ MultipleInheritance.cpp
@@ -4,10 +4,7 @@ using namespace std;
 class Occupation {
   public:
     int exp;
-  private:
-    Occupation() {
-      exp = 0;
-    }
+    Occupation() : exp(0) {}
 };
 
 class Fighter : public Occupation {
@@ -37,3 +34,44 @@ class Thief : public Occupation {
       cout << "release" << endl;
     }
 };
+
+class Bishop : public Mage, public Priest {};
+
+class Ninja : public Fighter, public Thief {};
+
+int main() {
+  Fighter taro;
+  Bishop hanako;
+  Ninja jiro;
+
+  cout << "Their's status" << endl;
+  cout << "=====================" << endl;
+  cout << "Name" << '\t' << "Job" << '\t' << "Exp" << endl;
+  cout << "Taro" << '\t' << "Fighter" << '\t' << taro.exp << endl;
+  cout << "Hanako" << '\t' << "Bishop" << '\t' << hanako.Mage::exp + hanako.Priest::exp << endl;
+  cout << "Jiro" << '\t' << "Ninja" << '\t' << jiro.Fighter::exp + jiro.Thief::exp << endl;
+  cout << "=====================" << endl;
+  cout << endl;
+
+  cout << "Taro:" << '\t';
+  taro.slash();
+  taro.exp += 10;
+
+  cout << "Hanako:" << '\t';
+  hanako.Priest::castSpell();
+  hanako.Priest::exp += 10;
+
+  cout <<  "Jiro:" << '\t';
+  jiro.release();
+  jiro.Thief::exp += 10;
+
+  cout << endl;
+
+  cout << "Their's status" << endl;
+  cout << "=====================" << endl;
+  cout << "Name" << '\t' << "Job" << '\t' << "Exp" << endl;
+  cout << "Taro" << '\t' << "Fighter" << '\t' << taro.exp << endl;
+  cout << "Hanako" << '\t' << "Bishop" << '\t' << hanako.Mage::exp + hanako.Priest::exp << endl;
+  cout << "Jiro" << '\t' << "Ninja" << '\t' << jiro.Fighter::exp + jiro.Thief::exp << endl;
+  cout << "=====================" << endl;
+}
