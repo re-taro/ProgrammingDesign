1fa38801dd268f63396ac73c60b6f8dc593cf46d
Re-taro <aminokacho@gmail.com>
2021-10-20 18:45:04 +0900

    :zap: ポリモーフィズムを意識した動的型変換

---
+++ TypeConversion/PolymorphismDynamicTypeConversion.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+#include<iostream>
+using namespace std;
+
+class Animal {
+  public:
+    virtual void say() = 0;
+};
+
+class Human : public Animal {
+  public:
+    void say() { speak(); }
+    void speak() {
+      cout << "speak" << endl;
+    }
+};
+
+class Dog : public Animal {
+  public:
+    void say() { bark(); }
+    void bark() {
+      cout << "bark" << endl;
+    }
+};
+
+void say(Animal* a) {
+  a -> say();
+}
+
+int main() {
+  Human human;
+  Dog dog;
+  say(&human);
+  say(&dog);
+}
