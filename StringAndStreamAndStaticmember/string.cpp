3aff4a61038c3cd78dcac894144828e9cfe13c70
Re-taro <aminokacho@gmail.com>
2021-12-18 00:09:30 +0900

    :sparkles: Understand about string

---
+++ StringAndStreamAndStaticmember/string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+  string str1;
+  string str2 = "Program Design";
+  string str3;
+  cin >> str1;
+  reverse(str2.begin(), str2.end());
+  str3 = str1 + str2;
+  cout << str3 << endl;
+}
