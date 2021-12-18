d59bae5e0d547c637a7eb768672a7d9544d72040
Re-taro <aminokacho@gmail.com>
2021-12-18 22:39:44 +0900

    :sparkles: output to data.txt

---
+++ StringAndStreamAndStaticmember/output2data.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(void) {
+  ofstream file;
+  file.open("data.txt");
+  double data;
+  string value;
+  while (true) {
+    getline(cin, value);
+    if ( value.length()) {
+      data = stod(value);
+      file << data << endl;
+      continue;
+    }
+    break;
+  }
+  file.close();
+}
